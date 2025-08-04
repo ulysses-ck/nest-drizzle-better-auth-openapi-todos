a65194fce365c4256f887c753caf9c9f9c6de675
Ulises <ulisesapaza.280@gmail.com>
2025-08-04 19:00:30 -0300

    chore: create dto for insert, update

--- src/db/zod-schema.ts
+++ src/db/zod-schema.ts
@@ -1,7 +1,11 @@
-import { createSelectSchema } from 'drizzle-zod';
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'drizzle-zod';
 import type z from 'zod';
 import { todoTable } from './schema';
 
 export type CreateTodoDto = z.infer<typeof createTodoSchema>;
+export type UpdateTodoDto = z.infer<typeof updateTodoSchema>;
+export type InsertTodoDto = z.infer<typeof insertTodoSchema>;
 
 export const createTodoSchema = createSelectSchema(todoTable);
+export const updateTodoSchema = createUpdateSchema(todoTable);
+export const insertTodoSchema = createInsertSchema(todoTable);
