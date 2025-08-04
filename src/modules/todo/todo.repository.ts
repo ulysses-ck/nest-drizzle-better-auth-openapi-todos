a610bff304e0f5513b162b8992c896643fef81e9
Ulises <ulisesapaza.280@gmail.com>
2025-08-04 17:46:03 -0300

    chore: format

--- src/modules/todo/todo.repository.ts
+++ src/modules/todo/todo.repository.ts
@@ -1,53 +1,53 @@
-import { Injectable } from "@nestjs/common";
-import type { TransactionHost } from "@nestjs-cls/transactional";
-import { eq } from "drizzle-orm";
-import type { DbTransactionAdapter } from "src/db/client";
-import { todoTable } from "src/db/schema";
+import { Injectable } from '@nestjs/common';
+import type { TransactionHost } from '@nestjs-cls/transactional';
+import { eq } from 'drizzle-orm';
+import type { DbTransactionAdapter } from 'src/db/client';
+import { todoTable } from 'src/db/schema';
 
 @Injectable()
 export class TodoRepository {
-  constructor(private readonly txHost: TransactionHost<DbTransactionAdapter>) {}
+	constructor(private readonly txHost: TransactionHost<DbTransactionAdapter>) {}
 
-  async find(id: string) {
-    const [todo] = await this.txHost.tx
-      .select()
-      .from(todoTable)
-      .where(eq(todoTable.id, id));
+	async find(id: string) {
+		const [todo] = await this.txHost.tx
+			.select()
+			.from(todoTable)
+			.where(eq(todoTable.id, id));
 
-    if (!todo) {
-      return null;
-    }
+		if (!todo) {
+			return null;
+		}
 
-    return todo;
-  }
+		return todo;
+	}
 
-  async all() {
-    return this.txHost.tx.select().from(todoTable);
-  }
+	async all() {
+		return this.txHost.tx.select().from(todoTable);
+	}
 
-  async createTodo(values: { isCompleted: boolean; text: string }) {
-    const [todo] = await this.txHost.tx
-      .insert(todoTable)
-      .values({ ...values })
-      .returning({ id: todoTable.id });
+	async createTodo(values: { isCompleted: boolean; text: string }) {
+		const [todo] = await this.txHost.tx
+			.insert(todoTable)
+			.values({ ...values })
+			.returning({ id: todoTable.id });
 
-    if (!todo.id) {
-      return null;
-    }
+		if (!todo.id) {
+			return null;
+		}
 
-    return todo;
-  }
+		return todo;
+	}
 
-  async removeTodo(values: { id: string }) {
-    const [todoRemoved] = await this.txHost.tx
-      .delete(todoTable)
-      .where(eq(todoTable.id, values.id))
-      .returning({ id: todoTable.id });
+	async removeTodo(values: { id: string }) {
+		const [todoRemoved] = await this.txHost.tx
+			.delete(todoTable)
+			.where(eq(todoTable.id, values.id))
+			.returning({ id: todoTable.id });
 
-    if (!todoRemoved.id) {
-      return null;
-    }
+		if (!todoRemoved.id) {
+			return null;
+		}
 
-    return todoRemoved;
-  }
+		return todoRemoved;
+	}
 }
