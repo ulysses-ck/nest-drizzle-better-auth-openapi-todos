05ef8112f8cbdb46cce460316a8833a11c92c4f9
Ulises <ulisesapaza.280@gmail.com>
2025-08-04 18:15:20 -0300

    feat: zod pipe

---
+++ src/zod-validation/zod-validation.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { createTodoSchema } from 'src/db/zod-schema';
+import { ZodValidationPipe } from './zod-validation.pipe';
+
+describe('ZodValidationPipe', () => {
+  it('should be defined', () => {
+    expect(new ZodValidationPipe(createTodoSchema)).toBeDefined();
+  });
+});
---
+++ src/zod-validation/zod-validation.pipe.ts
@@ -0,0 +1,16 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import type { ZodType } from 'zod/v4';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor (private readonly schema: ZodType) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    try {
+      const parsedValue = this.schema.parse(value);
+      return parsedValue;
+    } catch(error) {
+      throw new BadRequestException("Validation failed");
+    }
+  }
+}
