610e144ffcaf40969da2d0b117b9d965163343b0
Ulises <ulisesapaza.280@gmail.com>
2025-08-04 18:20:59 -0300

    chore: formatting issues

--- src/zod-validation/zod-validation.pipe.spec.ts
+++ src/zod-validation/zod-validation.pipe.spec.ts
@@ -2,7 +2,7 @@ import { createTodoSchema } from 'src/db/zod-schema';
 import { ZodValidationPipe } from './zod-validation.pipe';
 
 describe('ZodValidationPipe', () => {
-  it('should be defined', () => {
-    expect(new ZodValidationPipe(createTodoSchema)).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(new ZodValidationPipe(createTodoSchema)).toBeDefined();
+	});
 });
--- src/zod-validation/zod-validation.pipe.ts
+++ src/zod-validation/zod-validation.pipe.ts
@@ -1,16 +1,21 @@
-import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import {
+  type ArgumentMetadata,
+  BadRequestException,
+  Injectable,
+  type PipeTransform,
+} from '@nestjs/common';
 import type { ZodType } from 'zod/v4';
 
 @Injectable()
 export class ZodValidationPipe implements PipeTransform {
-  constructor (private readonly schema: ZodType) {}
+	constructor(private readonly schema: ZodType) {}
 
-  transform(value: unknown, metadata: ArgumentMetadata) {
-    try {
-      const parsedValue = this.schema.parse(value);
-      return parsedValue;
-    } catch(error) {
-      throw new BadRequestException("Validation failed");
-    }
-  }
+	transform(value: unknown, metadata: ArgumentMetadata) {
+		try {
+			const parsedValue = this.schema.parse(value);
+			return parsedValue;
+		} catch (error) {
+			throw new BadRequestException('Validation failed');
+		}
+	}
 }
