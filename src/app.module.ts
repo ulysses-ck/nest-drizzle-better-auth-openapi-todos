e34e2eb60933d6accc95de0ea49f5226bb535828
Ulises <ulisesapaza.280@gmail.com>
2025-08-04 16:35:28 -0300

    feat: include cls module and todoservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { ClsModule } from 'nestjs-cls';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
+import { TransactionalAdapterDrizzleOrm } from '@nestjs-cls/transactional-adapter-drizzle-orm';
+import { DB_PROVIDER } from './db/db.provider';
+import { TodosService } from './providers/todos/todos.service';
 
 @Module({
-	imports: [],
+	imports: [
+		DbModule,
+		ClsModule.forRoot({
+			global: true,
+			plugins: [
+				new ClsPluginTransactional({
+					imports: [DbModule],
+					adapter: new TransactionalAdapterDrizzleOrm({
+						drizzleInstanceToken: DB_PROVIDER,
+					}),
+				}),
+			],
+		}),
+	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [AppService, TodosService],
 })
 export class AppModule {}
