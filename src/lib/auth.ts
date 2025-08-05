74b560b3fa5689ad06e1d67562d2a010afd51db6
Ulises <ulisesapaza.280@gmail.com>
2025-08-05 12:12:40 -0300

    chore: add plugin openapi

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { openAPI } from 'better-auth/plugins';
 import * as authSchema from '@/db/auth-schema';
 import db from '@/db/client';
 
@@ -13,4 +14,5 @@ export const auth = betterAuth({
 	emailAndPassword: {
 		enabled: true,
 	},
+	plugins: [openAPI()],
 });
