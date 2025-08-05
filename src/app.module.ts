2f1f89953053d6de66911d3e0759ab13406f79cf
Ulises <ulisesapaza.280@gmail.com>
2025-08-05 12:09:46 -0300

    chore: include authmodule into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ClsPluginTransactional } from '@nestjs-cls/transactional';
 import { TransactionalAdapterDrizzleOrm } from '@nestjs-cls/transactional-adapter-drizzle-orm';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { ClsModule } from 'nestjs-cls';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
 import { DB_PROVIDER } from './db/db.provider';
+import { auth } from './lib/auth';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
@@ -22,6 +24,7 @@ import { TodosModule } from './todos/todos.module';
 				}),
 			],
 		}),
+		AuthModule.forRoot(auth),
 		TodosModule,
 	],
 	controllers: [AppController],
