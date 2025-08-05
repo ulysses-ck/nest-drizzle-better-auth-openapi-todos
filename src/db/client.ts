77ceda3f9db59770e7b4eff8655fe7d12273b86a
Ulises <ulisesapaza.280@gmail.com>
2025-08-04 23:33:38 -0300

    fix: update drizzle config
    schemas now are inlcuded

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,8 +1,14 @@
 import type { TransactionalAdapterDrizzleOrm } from '@nestjs-cls/transactional-adapter-drizzle-orm';
+import { todoTable } from '@/todos/entities/todo.entity';
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-const db = drizzle(process.env.DB_URL!);
+const db = drizzle({
+	connection: process.env.DATABASE_URL!,
+	schema: {
+		todoTable,
+	},
+});
 
 export type DB = typeof db;
 
