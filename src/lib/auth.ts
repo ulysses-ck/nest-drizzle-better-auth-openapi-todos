b7ecf6eab3a72f7199d98928b3b04e01983d1d4f
Ulises <ulisesapaza.280@gmail.com>
2025-08-05 11:16:14 -0300

    chore: format issues

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,16 @@
-import db from "@/db/client";
-import {betterAuth} from "better-auth";
-import {drizzleAdapter} from "better-auth/adapters/drizzle";
-import * as authSchema from "@/db/auth-schema";
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import * as authSchema from '@/db/auth-schema';
+import db from '@/db/client';
 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, {
-        provider: "pg",
-        schema: {
-            authSchema,
-        }
-    }),
-    emailAndPassword: {
-        enabled:true
-    }
-})
+	database: drizzleAdapter(db, {
+		provider: 'pg',
+		schema: {
+			authSchema,
+		},
+	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+});
