ce79e378ab7529602ddb1e47af0d9cac216d3aef
Ulises <ulisesapaza.280@gmail.com>
2025-08-04 23:31:17 -0300

    fix: correct path to entity

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: ["./src/todos/entities/todo.dto.ts"],
+  schema: ["./src/todos/entities/todo.entity.ts"],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
